bc18b7a1d2dd7eeae6c23b123a8079b9f188c41b
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-08-10 21:59:35 +0330

    Added facility.html

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const fs = require("fs");
 const app = express();
 const PORT = 4000;
 
+// HTML file paths
+
 const rootHTMLPath = path.join(__dirname, "html_files/root.html");
 const ssimionHTMLPath = path.join(__dirname, "html_files/ssimion.html");
 const theHTMLPath = path.join(__dirname, "html_files/the.html");
 const path2HTMLPath = path.join(__dirname, "html_files/path2.html");
+const facilityHTMLPath = path.join(__dirname, "html_files/facility.html");
+
+// Handling different routes
 
 app.get("/", (req, res) => {
   const content = fs.readFileSync(rootHTMLPath, "utf8");
@@ -28,6 +33,13 @@ app.get("/path2", (req, res) => {
   const content = fs.readFileSync(path2HTMLPath, "utf8");
   res.status(404).type("html").send(content);
 });
+
+app.get("/totallynotasecretfacility", (req, res) => {
+  const content = fs.readFileSync(facilityHTMLPath, "utf8");
+  res.status(404).type("html").send(content);
+});
+
+// Log to show server is running
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
