1676c247edfe3002da0ef3cde413384849a69a4a
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-08-10 17:35:20 +0330

    Probable fix of fs not finding path

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express");
+const path = require("path");
 const fs = require("fs");
 const app = express();
 const PORT = 4000;
 
-const rootHTMLPath = "html_files/rootHTML.html";
+const rootHTMLPath = path.join(__dirname, "html_files/rootHTML.html");
 
 app.get("/home", (req, res) => {
   res.status(200).send("Home sweet Home sweet !");
