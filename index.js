dd893d3e5b6e36f964b9b9ad59796111f50cf8ab
Ilia Abbasi <81512402+ilia-abbasi@users.noreply.github.com>
2025-08-10 21:00:38 +0330

    Added the.html and path2.html
    - Also added 404 handler

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ const PORT = 4000;
 
 const rootHTMLPath = path.join(__dirname, "html_files/root.html");
 const ssimionHTMLPath = path.join(__dirname, "html_files/ssimion.html");
-
-app.get("/home", (req, res) => {
-  res.status(200).send("Home sweet Home sweet !");
-});
+const theHTMLPath = path.join(__dirname, "html_files/the.html");
+const path2HTMLPath = path.join(__dirname, "html_files/path2.html");
 
 app.get("/", (req, res) => {
   const content = fs.readFileSync(rootHTMLPath, "utf8");
@@ -21,8 +19,29 @@ app.get("/ssimion", (req, res) => {
   res.status(200).type("html").send(content);
 });
 
+app.get("/the", (req, res) => {
+  const content = fs.readFileSync(theHTMLPath, "utf8");
+  res.status(200).type("html").send(content);
+});
+
+app.get("/path2", (req, res) => {
+  const content = fs.readFileSync(path2HTMLPath, "utf8");
+  res.status(404).type("html").send(content);
+});
+
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
+});
+
+// Handling 404 responses
+
+app.use((req, res, next) => {
+  res
+    .status(404)
+    .type("html")
+    .send(
+      "<h1>404 - Page Not Found</h1><p>The page you requested does not exist.</p>"
+    );
 });
 
 module.exports = app;
